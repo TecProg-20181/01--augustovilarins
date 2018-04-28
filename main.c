58247f96056ffb92d02108eb809f347268402cef
augustovilarins <augusto.vilarins@gmail.com>
2018-04-28 14:59:25 -0300

    Fixing blur and grey scale methods

--- main.c
+++ main.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 
 typedef struct _pixel {
-    unsigned short int r;
-    unsigned short int g;
-    unsigned short int b;
+    unsigned short int red;
+    unsigned short int green;
+    unsigned short int blue;
 } Pixel;
 
 typedef struct _image {
-    // [width][height][rgb]
-    // 0 -> r
-    // 1 -> g
-    // 2 -> b
-    unsigned short int pixel[512][512][3];
+    Pixel pixel[512][512];
     unsigned int image_width;
     unsigned int image_height;
 } Image;
 
-
-int max(int a, int b) {
-    if (a > b)
-        return a;
+int min(int a, int b){
+  if (a > b)
     return b;
+return a;
 }
+
+int max(int a, int b){
+  if (a > b)
+    return a;
+return b;
+}
+
 
 int compare_pixels(Pixel pixel1, Pixel pixel2) {
     if (pixel1.r == pixel2.r &&
@@ -33,50 +35,44 @@ int compare_pixels(Pixel pixel1, Pixel pixel2) {
 
 
 Image grey_scale(Image image) {
-    /*for (unsigned int i = 0; i < img.h; ++i) {
-        for (unsigned int j = 0; j < img.w; ++j) {
-            print("%u", img.pixel[i][j][0] + img.pixel[i][j][1] + img.pixel[i][j][2]);
-        }
-    }*/
 
-    for (unsigned int i = 0; i < image.h; ++i) {
+    for (unsigned int i = 0; i < image.image_height; ++i) {
         for (unsigned int j = 0; j < image.image_width; ++j) {
-            int media = image.pixel[i][j][0] +
-                        image.pixel[i][j][1] +
-                        image.pixel[i][j][2];
-            media /= 3;
-            image.pixel[i][j][0] = media;
-            image.pixel[i][j][1] = media;
-            image.pixel[i][j][2] = media;
+          int media = image.pixel[i][j].red +
+                      image.pixel[i][j].green +
+                      image.pixel[i][j].blue;
+          media /= 3;
+          image.pixel[i][j].red = media;
+          image.pixel[i][j].green = media;
+          image.pixel[i][j].blue = media;
         }
     }
 
     return image;
 }
 
-void blur(unsigned int h, unsigned short int pixel[512][512][3], int T, unsigned int image_width) {
-    for (unsigned int i = 0; i < h; ++i) {
+void blur(Image image, unsigned int blur_scale) {
+    for (unsigned int i = 0; i < image.image_height; ++i) {
         for (unsigned int j = 0; j < image_width; ++j) {
             Pixel media = {0, 0, 0};
 
-            int menor_h = (h - 1 > i + T/2) ? i + T/2 : h - 1;
-            int min_image_width = (image_width - 1 > j + T/2) ? j + T/2 : image_width - 1;
-            for(int x = (0 > i - T/2 ? 0 : i - T/2); x <= menor_h; ++x) {
-                for(int y = (0 > j - T/2 ? 0 : j - T/2); y <= min_image_width; ++y) {
+            int min_image_height = min(i + blur_scale/2, image.image_height - 1);
+            int min_image_width = min(i + blur_scale/2, image.image_width - 1);
+            for(unsigned int x = max(0, i - blur_scale/2); x <= min_image_height; ++x) {
+                for(unsigned int y = max(0,j - blur_scale/2); y <= min_image_width; ++y) {
                     media.r += pixel[x][y][0];
                     media.g += pixel[x][y][1];
                     media.b += pixel[x][y][2];
                 }
             }
 
-            // printf("%u", media.r)
-            media.r /= T * T;
-            media.g /= T * T;
-            media.b /= T * T;
+            media.red /= blur_scale * blur_scale;
+            media.green /= blur_scale * blur_scale;
+            media.blue /= blur_scale * blur_scale;
 
-            pixel[i][j][0] = media.r;
-            pixel[i][j][1] = media.g;
-            pixel[i][j][2] = media.b;
+            pixel[i][j].red = media.red;
+            pixel[i][j].green = media.green;
+            pixel[i][j].blue = media.blue;
         }
     }
 }
